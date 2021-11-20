05eaad1ad091e1fe4785efb792825242a256ca42
Soni <soni801_@outlook.com>
2021-11-20 22:10:16 +0100

    feat: implement copy button functionality

--- main.js
+++ main.js
@@ -3,16 +3,27 @@ const responseList = document.querySelector("#response-list");
 
 fetch("/responses.json").then(r => r.json()).then(responses => responses.forEach(response =>
 {
+    // Load responses
     responseList.innerHTML += `
         <div id="${response.id}" class="response">
             <img src="${response.src}" alt="${response.name}">
             <div>
                 <h3>${response.name}</h3>
-                <span class="copy-button"></span>
+                <span class="copy-button" content="${response.src}"></span>
             </div>
         </div>
     `;
 })).then(() =>
 {
+    // Load copy button
     $(".copy-button").load("/assets/icons/copy.svg");
+
+    // Add event listener to copy button
+    document.querySelectorAll(".copy-button").forEach(e =>
+    {
+        e.addEventListener("click", () =>
+        {
+            navigator.clipboard.writeText(e.getAttribute("content"));
+        });
+    });
 });

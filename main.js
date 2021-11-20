c0f2e5d19cec633f3fc05b49a58cccb1eb13941b
Soni <soni801_@outlook.com>
2021-11-20 21:57:18 +0100

    feat: add copy button

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const responseList = document.querySelector("#response-list");
 fetch("/responses.json").then(r => r.json()).then(responses => responses.forEach(response =>
 {
     responseList.innerHTML += `
-        <div id="${response.id}">
+        <div id="${response.id}" class="response">
             <img src="${response.src}" alt="${response.name}">
-            <h3>${response.name}</h3>
+            <div>
+                <h3>${response.name}</h3>
+                <span class="copy-button"></span>
+            </div>
         </div>
     `;
-}));
+})).then(() =>
+{
+    $(".copy-button").load("/assets/icons/copy.svg");
+});
